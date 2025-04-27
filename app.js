b81d838c25546769c45f5f199a2d6d5a205253b8
draxdev <davipeterson173@gmail.com>
2025-04-27 17:14:00 -0400

    fix prisma class

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient
+const prisma = new PrismaClient()
 const app = express()
 const port = process.env.PORT || 4000 
 
